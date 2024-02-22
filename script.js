6a65adad46ab00ce25c64cb886b6fed438829402
ADITYAMANDKE <adityamandke23@gmail.com>
2024-02-22 21:05:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
+var jokeSetup;
+var jokePunchline;
 function getJoke()
 {
     const url = "https://official-joke-api.appspot.com/random_joke";
     fetch(url)
             .then(response => response.json()).then(data => {
-                const jokeSetup = data.setup;
-                const jokePunchline = data.punchline;
+                jokeSetup = data.setup;
+                jokePunchline = data.punchline;
                 document.getElementById("joke-text").innerText =`${data.setup} ${data.punchline}`;
-                const favbtn = document.createElement("button");
-                favbtn.innerText = "Add to Favourite";
-                favbtn.onclick = () => {
-                  addtoFavourites(jokeSetup,jokePunchline);
-                  favbtn.remove();
-                }
                 document.getElementById("joke-container").appendChild(favbtn)}).catch(error => {console.log("error in api ".error);document.getElementById("joke-text").innerText = "Failed to fetch Joke";});  
 }
 
-function addtoFavourites(jokeSetup,jokePunchline)
+function addtoFavourites()
 {
-
+    const favbtn = document.getElementById("favbtn");
+    //favbtn.innerText = "Add to Favourite";
     const favJokeContainer = document.getElementById("favorites-container");
     const favJoke = document.createElement("div");
     const jokeContent = document.createElement("p");
